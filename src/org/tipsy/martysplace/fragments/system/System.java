0e9cc9509060d0a08c46c68cdda15335d09cf062
alliancesparx <alliancesparx@gmail.com>
2020-04-12 11:47:58 +0100

    Martys Place is open for business!
    First drink is on the boss!!

---
+++ src/org/tipsy/martysplace/fragments/system/System.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2018 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tipsy.martysplace.fragments.system;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.text.TextUtils;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.R;
+import com.android.settings.carbon.CustomSettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import org.tipsy.martysplace.preference.AppMultiSelectListPreference;
+import org.tipsy.martysplace.preference.ScrollAppsViewPreference;
+
+@SearchIndexable
+public class System extends CustomSettingsPreferenceFragment implements Preference.OnPreferenceChangeListener, Indexable {
+    private static final String TAG = "System";
+    private static final String SMART_PIXELS = "smart_pixels";
+    private static final String GLOBAL_ACTIONS = "global_actions";
+    private static final String AGGRESSIVE_BATTERY ="aggressive_battery";
+    private static final String ADVANCED_REBOOT = "advanced_reboot";
+    private static final String CHARGING_ANIMATION = "charging_animation";
+    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
+    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+    private static final String DASHBOARD_SUGESSTIONS = "enable_suggestions";
+    private static final String DASHBOARD_CONDITIONS = "enable_conditions";
+    private static final String KEY_SYSTEM_POWER_SAVE_PREFERENCE_CATEGORY = "system_power_save_preference_category";
+
+    private AppMultiSelectListPreference mAspectRatioAppsSelect;
+    private ScrollAppsViewPreference mAspectRatioApps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        addPreferencesFromResource(R.xml.system);
+        addCustomPreference(findPreference(ADVANCED_REBOOT), SECURE_TWO_STATE, STATE_ON);
+        addCustomPreference(findPreference(CHARGING_ANIMATION), SYSTEM_TWO_STATE, STATE_OFF);
+        addCustomPreference(findPreference(KEY_ASPECT_RATIO_APPS_ENABLED), SYSTEM_TWO_STATE, STATE_OFF);
+        addCustomPreference(findPreference(DASHBOARD_SUGESSTIONS), SYSTEM_TWO_STATE, STATE_ON);
+        addCustomPreference(findPreference(DASHBOARD_CONDITIONS), SYSTEM_TWO_STATE, STATE_ON);
+
+        final PreferenceCategory powerSaveCategory =
+                (PreferenceCategory) getPreferenceScreen().findPreference(KEY_SYSTEM_POWER_SAVE_PREFERENCE_CATEGORY);
+        final boolean enableSmartPixels =
+                getResources().getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
+        Preference smartPixels = powerSaveCategory.findPreference(SMART_PIXELS);
+
+        if (!enableSmartPixels){
+            powerSaveCategory.removePreference(smartPixels);
+        }
+
+        final PreferenceCategory aspectRatioCategory =
+                (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
+        final boolean supportMaxAspectRatio =
+                getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+        if (!supportMaxAspectRatio) {
+                getPreferenceScreen().removePreference(aspectRatioCategory);
+        } else {
+        mAspectRatioAppsSelect =
+                (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
+        mAspectRatioApps =
+                (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
+        final String valuesString = Settings.System.getString(getContentResolver(),
+                Settings.System.ASPECT_RATIO_APPS_LIST);
+        List<String> valuesList = new ArrayList<String>();
+        if (!TextUtils.isEmpty(valuesString)) {
+            valuesList.addAll(Arrays.asList(valuesString.split(":")));
+            mAspectRatioApps.setVisible(true);
+            mAspectRatioApps.setValues(valuesList);
+        } else {
+            mAspectRatioApps.setVisible(false);
+        }
+        mAspectRatioAppsSelect.setValues(valuesList);
+        mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAspectRatioAppsSelect) {
+            Collection<String> valueList = (Collection<String>) newValue;
+            mAspectRatioApps.setVisible(false);
+            if (valueList != null) {
+                Settings.System.putString(getContentResolver(),
+                        Settings.System.ASPECT_RATIO_APPS_LIST, TextUtils.join(":", valueList));
+                mAspectRatioApps.setVisible(true);
+                mAspectRatioApps.setValues(valueList);
+            } else {
+                Settings.System.putString(getContentResolver(),
+                Settings.System.ASPECT_RATIO_APPS_LIST, "");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                                                                            boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.system;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    keys.add(SMART_PIXELS);
+                    keys.add(AGGRESSIVE_BATTERY);
+                    keys.add(GLOBAL_ACTIONS);
+                    return keys;
+                }
+            };
+}
